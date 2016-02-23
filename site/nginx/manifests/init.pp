8893665ea65ce2a05143a2188ac9d91efa9b8b22
Karel Brezina <karel.brezina@puppetlabs.com>
2016-02-23 14:32:26 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,4 +5,38 @@ class nginx {
     
   }
 
+file { '/etc/nginx/nginx.conf':
+  ensure  => 'file',
+  source  => 'puppet:///modules/nginx/nginx.conf',
+# content => '{md5}060207123b3c5c5189cbe5259e1d5c2f',
+# ctime   => '2016-02-23 13:22:16 +0000',
+  group   => '0',
+  mode    => '0644',
+#  mtime  => '2016-01-26 18:15:47 +0000',
+  owner   => '0',
+ # type   => 'file',
+ require  => Package['nginx'],
+}
+file { '/etc/nginx/conf.d/default.conf':
+  ensure => 'file',
+  source=> 'puppet:///modules/nginx/nginx.conf',
+  require => Package['nginx'],
+}
+file { '/var/www':
+  ensure => directory,
+}
+file { '/var/www/index.html':
+  ensure  => file,
+  source  => 'puppet:///modules/nginx/index.html',
+
+}
+service{ 'nginx':
+  ensure  => 'running',
+  enable => 'false',
+  require => [
+              File['/etc/nginx/conf.d/default.conf'],
+              File['/etc/nginx/nginx.conf']
+              ]
+  
+  }
 }
