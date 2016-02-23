8e9e9ebb1b11f10a6b097a70a8b8ad6035eb0d1a
Karel Brezina <karel.brezina@puppetlabs.com>
2016-02-23 13:50:40 +0100

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,17 @@
+class memcached {
+
+package { 'memcached':
+  ensure => present,
+}
+
+file { '/etc/sysconfig/memcached':
+  ensure => present,
+  source => "puppet:///modules/memcached/memcached"
+  require => Package['memcached'],
+}
+
+service { 'memcached':
+  ensure => running,
+  subscribe => File['/etc/sysconfig/memcached'],
+}
+}
