3b942ae0f84c46b017d8e499c444a243544591c9
Karel Brezina <karel.brezina@puppetlabs.com>
2016-02-25 10:12:06 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,43 +1,71 @@
-class nginx {
+class nginx{
+$package_name = 'nginx'
+$owner        = 'root'
+$group        = $owner
+$doc_root     = '/var/www'
+$conf_dir     = '/etc/nginx'
+$block_dir    = "${conf_dir}/conf.d"
+$logdir       = "/var/log/${package_name}"
+$service_name = $package_name
 
-  $package = 'nginx'
-  
-  File {
-    owner => '0',
-    group => '0',
-    mode  => '0644',
-  }
+$service_user = $::osfamily ? {
+  'RedHat'  => $package_name,
+  'Debian'  => 'www-data',
+  default   => $package_name,
+}
 
-  package { $package:
-    ensure => present,
-    
-  }
+File{
+  owner => $owner,
+  group => $group,
+  mode  =>  '0644',
+}
 
-  file { '/etc/nginx/nginx.conf':
-    ensure  => 'file',
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require  => Package['nginx'],
-  }
-  
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => 'file',
-    source=> 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-  }
-  
-  file { '/var/www':
-    ensure => directory,
+  package{ $package_name :
+    ensure  => present,
   }
   
-  file { '/var/www/index.html':
-    ensure  => file,
-    source  => 'puppet:///modules/nginx/index.html',
-  }
+#file { '/etc/nginx/nginx.conf':
+file { "${conf_dir}/${package_name}.conf":
+  ensure  => 'file',
+  content  => template('nginx/nginx.conf.erb'),
+  #source  => 'puppet:///modules/nginx/nginx.conf',
+  require => Package[$package_name],
+}
 
-  service{ $package:
-    ensure    => 'running',
-    enable    => 'false',
-    subscribe => File['/etc/nginx/conf.d/default.conf', '/etc/nginx/nginx.conf'],
-    require   => File['/var/www/index.html'],
+#file { '/etc/nginx/conf.d/default.conf':
+file { "${block_dir}/default.conf":
+  ensure  => 'file',
+  content  => template('nginx/default.conf.erb'),
+  #source  => 'puppet:///modules/nginx/default.conf',
+  require => Package[$package_name],
+}
+
+#file { '/var/www':
+file { $doc_root:
+  ensure => directory,
+}
+
+#file { '/var/www/index.html':
+file { "${doc_root}/index.html":
+  ensure  => file,
+  source  => 'puppet:///modules/nginx/index.html',
+
+}
+service{ $service_name :
+  ensure    => 'running',
+  enable    => 'false',
+  subscribe => [
+              File["${block_dir}/default.conf"],
+              File["${conf_dir}/${package_name}.conf"]
+              ]
   }
+#service{ $package :
+#  ensure    => 'running',
+#  enable    => 'false',
+#  subscribe => [
+#              File['/etc/nginx/conf.d/default.conf'],
+#              File['/etc/nginx/nginx.conf']
+#              ]
+#  }
+
 }
