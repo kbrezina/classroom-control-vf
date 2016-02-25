124c53582cbc9f037dcffe13b5f7022bc1e99630
Karel Brezina <karel.brezina@puppetlabs.com>
2016-02-25 11:57:27 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,21 +1,9 @@
 class nginx(
-  $root = '/var/www'
-)
+  $root = $::nginx::params::default_doc_root
+) inherits params
 {
-$package_name = 'nginx'
-$owner        = 'root'
-$group        = $owner
-$doc_root     = $root
-$conf_dir     = '/etc/nginx'
-$block_dir    = "${conf_dir}/conf.d"
-$logdir       = "/var/log/${package_name}"
-$service_name = $package_name
 
-$service_user = $::osfamily ? {
-  'RedHat'  => $package_name,
-  'Debian'  => 'www-data',
-  default   => $package_name,
-}
+$doc_root = $root
 
 File{
   owner => $owner,
