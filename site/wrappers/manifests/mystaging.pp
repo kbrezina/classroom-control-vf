85537304a30599ed9487939c6e13b05b0a174bde
Karel Brezina <karel.brezina@puppetlabs.com>
2016-02-25 14:04:54 +0100

    Create mystaging.pp

---
+++ site/wrappers/manifests/mystaging.pp
@@ -0,0 +1,16 @@
+class wrappers::mystaging{
+
+  class { 'staging':
+    path => '/tmp/staging',
+  }
+  
+  staging::file { 'sample.tar.gz':
+    source => 'puppet:///modules/staging/sample.tar.gz'
+  }
+
+  staging::extract { 'sample.tar.gz':
+    target  => '/tmp/staging',
+    creates => '/tmp/staging/sample',
+    require => Staging::File['sample.tar.gz'],
+  }
+}
